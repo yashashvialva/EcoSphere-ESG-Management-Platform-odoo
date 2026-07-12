290a678ffe1ef743259cecd353c46c8b4a53cb80
yashashvialva <alvayashashvi@gmail.com>
2026-07-12 11:49:40 +0530

    feat: add esg goal routes

--- backend/src/modules/environmental/routes/environmentalRoutes.js
+++ backend/src/modules/environmental/routes/environmentalRoutes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const emissionFactorController = require('../controllers/emissionFactorController');
 const carbonTransactionController = require('../controllers/carbonTransactionController');
+const esgGoalController = require('../controllers/esgGoalController');
 const { validate } = require('../../../../middleware/validate');
 const { authenticate } = require('../../../../middleware/auth');
 const { authorize } = require('../../../../middleware/authorize');
 const { createEmissionFactorSchema, updateEmissionFactorSchema } = require('../validators/emissionFactorValidator');
 const { createCarbonTransactionSchema } = require('../validators/carbonTransactionValidator');
+const { createEsgGoalSchema, updateEsgGoalSchema } = require('../validators/esgGoalValidator');
 
 // Apply authentication to all routes
 router.use(authenticate);
@@ -56,6 +58,39 @@ router.post(
   authorize('environmental.manage'),
   validate(createCarbonTransactionSchema),
   carbonTransactionController.create
+);
+
+// --- ESG Goals ---
+router.get(
+  '/esg-goals',
+  authorize('environmental.read'),
+  esgGoalController.getAll
+);
+
+router.get(
+  '/esg-goals/:id',
+  authorize('environmental.read'),
+  esgGoalController.getById
+);
+
+router.post(
+  '/esg-goals',
+  authorize('environmental.manage'),
+  validate(createEsgGoalSchema),
+  esgGoalController.create
+);
+
+router.patch(
+  '/esg-goals/:id',
+  authorize('environmental.manage'),
+  validate(updateEsgGoalSchema),
+  esgGoalController.update
+);
+
+router.delete(
+  '/esg-goals/:id',
+  authorize('environmental.manage'),
+  esgGoalController.delete
 );
 
 module.exports = router;
