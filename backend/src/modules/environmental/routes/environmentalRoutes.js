4b884c7bf1c3dcb799bfa5509d909118a672528c
yashashvialva <alvayashashvi@gmail.com>
2026-07-12 11:52:35 +0530

    feat: add product profile routes

--- backend/src/modules/environmental/routes/environmentalRoutes.js
+++ backend/src/modules/environmental/routes/environmentalRoutes.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const emissionFactorController = require('../controllers/emissionFactorController');
 const carbonTransactionController = require('../controllers/carbonTransactionController');
 const esgGoalController = require('../controllers/esgGoalController');
+const productProfileController = require('../controllers/productProfileController');
 const { validate } = require('../../../../middleware/validate');
 const { authenticate } = require('../../../../middleware/auth');
 const { authorize } = require('../../../../middleware/authorize');
 const { createEmissionFactorSchema, updateEmissionFactorSchema } = require('../validators/emissionFactorValidator');
 const { createCarbonTransactionSchema } = require('../validators/carbonTransactionValidator');
 const { createEsgGoalSchema, updateEsgGoalSchema } = require('../validators/esgGoalValidator');
+const { createProductProfileSchema, updateProductProfileSchema } = require('../validators/productProfileValidator');
 
 // Apply authentication to all routes
 router.use(authenticate);
@@ -91,6 +93,39 @@ router.delete(
   '/esg-goals/:id',
   authorize('environmental.manage'),
   esgGoalController.delete
+);
+
+// --- Product Profiles ---
+router.get(
+  '/product-profiles',
+  authorize('environmental.read'),
+  productProfileController.getAll
+);
+
+router.get(
+  '/product-profiles/:id',
+  authorize('environmental.read'),
+  productProfileController.getById
+);
+
+router.post(
+  '/product-profiles',
+  authorize('environmental.manage'),
+  validate(createProductProfileSchema),
+  productProfileController.create
+);
+
+router.patch(
+  '/product-profiles/:id',
+  authorize('environmental.manage'),
+  validate(updateProductProfileSchema),
+  productProfileController.update
+);
+
+router.delete(
+  '/product-profiles/:id',
+  authorize('environmental.manage'),
+  productProfileController.delete
 );
 
 module.exports = router;
