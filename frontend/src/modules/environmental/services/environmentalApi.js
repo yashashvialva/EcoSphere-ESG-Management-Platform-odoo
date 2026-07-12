77403ecc8cbcfb78d23dcc31145fe0d411f5403a
yashashvialva <alvayashashvi@gmail.com>
2026-07-12 11:53:04 +0530

    feat: add api methods for goals and profiles

--- frontend/src/modules/environmental/services/environmentalApi.js
+++ frontend/src/modules/environmental/services/environmentalApi.js
@@ -37,6 +37,48 @@ const environmentalApi = {
     const response = await api.post('/environmental/carbon-transactions', data);
     return response.data;
   },
+
+  // ESG Goals
+  getEsgGoals: async (params) => {
+    const response = await api.get('/environmental/esg-goals', { params });
+    return response.data;
+  },
+
+  createEsgGoal: async (data) => {
+    const response = await api.post('/environmental/esg-goals', data);
+    return response.data;
+  },
+
+  updateEsgGoal: async (id, data) => {
+    const response = await api.patch(`/environmental/esg-goals/${id}`, data);
+    return response.data;
+  },
+
+  deleteEsgGoal: async (id) => {
+    const response = await api.delete(`/environmental/esg-goals/${id}`);
+    return response.data;
+  },
+
+  // Product Profiles
+  getProductProfiles: async (params) => {
+    const response = await api.get('/environmental/product-profiles', { params });
+    return response.data;
+  },
+
+  createProductProfile: async (data) => {
+    const response = await api.post('/environmental/product-profiles', data);
+    return response.data;
+  },
+
+  updateProductProfile: async (id, data) => {
+    const response = await api.patch(`/environmental/product-profiles/${id}`, data);
+    return response.data;
+  },
+
+  deleteProductProfile: async (id) => {
+    const response = await api.delete(`/environmental/product-profiles/${id}`);
+    return response.data;
+  },
 };
 
 export default environmentalApi;
